7ed050885ba968e6e355e5d36305baea8efa693d
Leandro S Lopes <31078967+leandroslopes@users.noreply.github.com>
2023-06-12 16:07:16 -0300

    Create Catalog_Client_Script.js

---
+++ Catalog_Client_Script.js
@@ -0,0 +1,30 @@
+function onChange(control, oldValue, newValue, isLoading) {
+    if (isLoading) {
+        return;
+    }
+
+    if (newValue === "") {
+
+        g_form.clearValue('groups');
+    }
+
+    if (oldValue == newValue) {
+		
+        g_form.clearValue('groups');
+    }
+
+    var userSysId = g_form.getValue('choose_a_user');
+	
+    var glideAjax = new GlideAjax('MyWidgetsGroups');
+    glideAjax.addParam('sysparm_name', 'listString');
+    glideAjax.addParam('sysparm_user_sys_id', userSysId.toString());
+    glideAjax.getXML(GroupsParse);
+
+    function GroupsParse(response) {
+
+        var answer = response.responseXML.documentElement.getAttribute("answer");
+		
+        g_form.setValue('groups', JSON.parse(answer));
+    }
+
+}
